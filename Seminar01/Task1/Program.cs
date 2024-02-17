3f1be8feb845361a67b2f14972677742855bea7a
Wokrats <wokrats2@gmail.com>
2024-02-17 15:54:53 +0500

    Add Seminar Task1

---
+++ Seminar01/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Add Num1: ");
+int Num1 = int.Parse(Console.ReadLine());
+Console.Write("Add Num2: ");
+int Num2 = int.Parse(Console.ReadLine());
+if (Num2 == Num1 * Num1)
+{
+    Console.Write("Верно");
+}
+else
+{
+    Console.Write("Неверно");
+}
