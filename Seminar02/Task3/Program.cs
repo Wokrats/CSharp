e1c4ce933d3ad506e958c7d0758079e58aab6f58
Wokrats <wokrats2@gmail.com>
2024-02-21 20:00:35 +0500

    Add Sem02 Task3

--- Seminar02/Task3/Program.cs
+++ Seminar02/Task3/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Add first number: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Add second number: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1%num2 == 0)
+{
+    Console.Write("Yes");
+}
+else
+{
+    Console.Write(num1%num2);
+}
