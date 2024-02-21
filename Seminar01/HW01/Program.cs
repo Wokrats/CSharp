94d7c14b5903a7bc103bb6fbde2c074f000c8768
Wokrats <wokrats2@gmail.com>
2024-02-21 18:52:35 +0500

    add sem01 and sem02

---
+++ Seminar01/HW01/Program.cs
@@ -0,0 +1,53 @@
+﻿public class Answer {
+    
+  static int FindMax(int a, int b, int c)
+    {
+      // Введите свое решение ниже
+       
+       if (b>=a)
+        {
+            if (c>=a)
+            {
+                return c;
+            }
+            else
+            {
+                return b;
+            }
+        }
+       else
+       {
+            if (c>=a)
+            {
+                return c;
+            }
+            else
+            {
+                return a;
+            }
+       }
+          
+
+
+    }
+
+  // Не удаляйте и не меняйте метод Main! 
+  static public void Main(string[] args) {
+        int a, b, c;
+
+        if (args.Length >= 3) {
+           a = int.Parse(args[0]);
+           b = int.Parse(args[1]);
+           c = int.Parse(args[2]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+           a = 3;
+           b = 6;
+           c = 9;
+        }
+
+        // Не удаляйте строки ниже
+        int result = FindMax(a, b, c);
+        System.Console.WriteLine($"{result}");
+    }
+}
--- Seminar01/Task1/Program.cs
+++ Seminar01/Task1/Program.cs
@@ -2,11 +2,27 @@
 int Num1 = int.Parse(Console.ReadLine());
 Console.Write("Add Num2: ");
 int Num2 = int.Parse(Console.ReadLine());
-if (Num2 == Num1 * Num1)
+Console.Write("Add Num3: ");
+int Num3 = int.Parse(Console.ReadLine());
+if (Num2 > Num1)
 {
-    Console.Write("Верно");
+    if (Num3 > Num2)
+    {
+        Console.Write(Num3);
+    }
+    else
+    {
+        Console.Write(Num2);
+    }
 }
 else
 {
-    Console.Write("Неверно");
+    if (Num1 > Num3)
+    {
+        Console.Write(Num1);
+    }
+    else
+    {
+        Console.Write(Num3);
+    }
 }
