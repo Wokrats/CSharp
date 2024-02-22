21dbee4efa42359d3a92a6777c7855d90143515a
Wokrats <wokrats2@gmail.com>
2024-02-22 21:43:28 +0500

    Add Les2 Task2 and Task3

---
+++ Lesson02/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// int N = 10;
+int[] arr1 = {1,3,6,23,5,6,7,3,6,10};
+int i = 0;
+while (i<arr1.Length)
+{
+    if (arr1[i] % 2 == 0)
+    {
+        Console.Write($"{arr1[i]} ");
+    }
+    i ++;
+}
+
---
+++ Lesson02/Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿// int N = 10;
+int[] arr1 = {2,5,7,4,8,2,23,56,1,98};
+int i = 0;
+int max = arr1[i];
+while (i < arr1.Length)
+{
+    if (max < arr1[i])
+    {
+        max = arr1[i];
+    }
+    i ++;
+}
+Console.WriteLine(max);
+
+max = arr1[0];
+for (int j = 0; j < arr1.Length; j ++)
+{
+    if (max < arr1[j])
+    {
+        max = arr1[j];
+    }
+}
+Console.WriteLine(max);
+
+max = arr1[0];
+foreach (int e in arr1)
+{
+    if (max < e)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);
