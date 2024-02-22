7740571433e266b9287124c4e92b0efce542f57b
Wokrats <wokrats2@gmail.com>
2024-02-22 21:13:32 +0500

    Add Les2 Task1

---
+++ Lesson02/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿int N = 5;
+int[] arr = new int[N];
+int i = 0;
+while (i < arr.Length)
+{
+    arr[i] = i + 1;
+    // Console.Write(arr[i]);
+    // Console.Write(" ");
+    Console.Write($"{arr[i]} ");
+    i++;
+}
