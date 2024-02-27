7a191af9442e893a2a184923b060943cac17e3e1
Wokrats <wokrats2@gmail.com>
2024-02-27 22:09:55 +0500

    Add Sem4 Task2&Task3

--- Seminar04/Task2/Program.cs
+++ Seminar04/Task2/Program.cs
@@ -3,3 +3,36 @@
 //    Найдите количество чисел, которые
 //    оканчиваются на 1 и делятся нацело на 7.
 
+int[] CreateArray(int min, int max, int size) 
+{
+    Random rnd = new Random(); 
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++) 
+    {
+        array[i] = rnd.Next(min, max); 
+    }
+    return array; 
+}
+int CountNum(int[] array)
+{
+    int count = 0;
+    foreach (int num in array)
+    {
+        if (num % 10 == 1 && num % 7 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write("Add final array value: ");
+int max = int.Parse(Console.ReadLine()!);
+Console.Write("Add initial array value: ");
+int min = int.Parse(Console.ReadLine()!);
+Console.Write("Add array length: ");
+int size = int.Parse(Console.ReadLine()!);
+
+int[] result = CreateArray( min, max, size);
+Console.WriteLine(string.Join(" ", result));
+Console.WriteLine(CountNum(result));
--- Seminar04/Task3/Program.cs
+++ Seminar04/Task3/Program.cs
@@ -4,5 +4,39 @@
 //    массива. Старший разряд числа находится на 0-м индексе,
 //    младший – на последнем.
 
+int[] CreateArray(int min, int max, int size)
+{
+    Random rnd = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+    return array;
+}
 
+int AssemblyNum(int[] array)
+{
+    string count = "";
+    foreach (int num in array)
+    {
+        count = count + num;
+    }
+    return int.Parse(count);
+}
+
+int max = 9;
+int min = 0;
+Console.Write("Add array length: ");
+int size = int.Parse(Console.ReadLine()!);
+if (size > 8)
+{
+    Console.Write("Enter a number less than 8");
+}
+else
+{
+    int[] result = CreateArray( min, max, size);
+    Console.WriteLine(string.Join(" ", result));
+    Console.WriteLine(AssemblyNum(result));
+}
 
