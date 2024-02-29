e52e0740267cf1334b3c125f9cf1a20c7c2b1b41
Wokrats <wokrats2@gmail.com>
2024-02-29 21:41:25 +0500

    Add Les4 Task3

---
+++ Lesson04/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Считать с консоли строку, состоящую из латинских цифр и букв
+// Сформировать новую строку, состоящую из букв исходной строки
+
+string GetLetter(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+
+Console.Write("Введите строку из цифр и латинских букв: ");
+string demo_s = Console.ReadLine()!;
+Console.WriteLine(GetLetter(demo_s));
+
