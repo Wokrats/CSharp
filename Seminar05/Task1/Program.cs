c5ec9155496c95664f32bae7098e452e0f683e54
Wokrats <wokrats2@gmail.com>
2024-03-02 12:23:46 +0500

    Add Sem5 Task1,2

---
+++ Seminar05/Task1/Program.cs
@@ -0,0 +1,61 @@
+﻿// 1. Задайте двумерный массив. Найдите элементы, у которых оба
+//    индекса чётные, и замените эти элементы на их квадраты.
+
+int[,] CreateMatrix(int rowCount, int columnsCount)
+{
+    int[,] matrix = new int[rowCount, columnsCount];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(2, 10);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} "); //интерполяция строк
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SquareElemEvenIndex(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                matrix[i, j] *= matrix[i, j];
+            }
+        }
+    }
+    return matrix;
+}
+
+
+
+Console.Write("Enter count of rows: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Enter count of columns: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] DoubleArr = CreateMatrix(rows, columns);
+ShowMatrix(DoubleArr);
+Console.WriteLine();
+int[,] NewDoubleArr = SquareElemEvenIndex(DoubleArr);
+ShowMatrix(NewDoubleArr);
+
+
+
+
+
---
+++ Seminar05/Task2/Program.cs
@@ -0,0 +1,52 @@
+﻿// 2. Задайте двумерный массив. Найдите сумму элементов,
+//    находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+int[,] CreateMatrix(int rowCount, int columnsCount)
+{
+    int[,] matrix = new int[rowCount, columnsCount];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} "); //интерполяция строк
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Enter count of rows: ");
+int rowCount = int.Parse(Console.ReadLine()!);
+Console.Write("Enter count of columns: ");
+int columnsCount = int.Parse(Console.ReadLine()!);
+
+int SumDiag(int[,] DoubleArr)
+{
+    int sum = 0;
+    for (int i = 0; i < DoubleArr.GetLength(0); i++)
+    {
+        for (int j = 0; j < DoubleArr.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum = sum + DoubleArr[i, j];
+            }
+        }
+    }
+    return sum;
+}
+int[,] Arr = CreateMatrix(rowCount, columnsCount);
+ShowMatrix(Arr);
+Console.Write(SumDiag(Arr));
