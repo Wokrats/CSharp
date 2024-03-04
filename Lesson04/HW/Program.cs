83e5c095ac8d7a12221ff9b072d93c4c815128c2
Wokrats <wokrats2@gmail.com>
2024-03-04 21:40:33 +0500

    Add Les4 HW

---
+++ Lesson04/HW/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] CreateNumArray(string firststring)
+{
+    char[] charArray = firststring.ToCharArray();
+    int[] digitArray = new int[charArray.Length];
+    int digitCount = 0;
+    foreach (char e in charArray)
+    {
+        if (char.IsDigit(e))
+        {
+            digitArray[digitCount] = int.Parse(e.ToString());
+            digitCount ++;
+        }
+    }
+    return digitArray;
+}
+
+Console.Write("Введите строку из цифр и латинских букв: ");
+string demo_str = Console.ReadLine()!;
+int[] digitArray = CreateNumArray(demo_str);
+foreach (int i in digitArray)
+{
+    Console.Write($"{i}");
+}
